c9f8634d62a393b28918ec21bee6f7cd24e93a1b
Vijaymd0007 <vijaymd0007@gmail.com>
2023-09-13 23:30:10 +0530

    Updated media querries

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ downloadButton.addEventListener('click', () => {
     const resumeURL = './images/resume.pdf'; // Replace with your resume file URL
     window.open(resumeURL, '_blank');
 });
+
+
+const blurDivs = document.querySelectorAll(".blur-div")
+
+blurDivs.forEach(div=>{
+  const img = div.querySelector("img")
+
+  function loaded(){
+
+  }
+  if(img.complete){
+    loaded()
+  }
+  else{
+    img.addEventListener("load",loaded)
+  }
+})
